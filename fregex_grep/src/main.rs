f74529475aafdc961c33646880cc90bca9b4fa80
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2022-12-08 12:46:14 +0100

    Extend binary functionality

--- fregex_grep/src/main.rs
+++ fregex_grep/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use fregex::{Regex, MultiRegex, RegexMatcher};
-use text::{read_text, read_text_lines};
+use regex::Regex as OriginalRegex;
+use text::read_text;
 use std::process::exit;
 
 use crate::args::Args;
@@ -8,14 +9,7 @@ use crate::args::Args;
 mod args;
 mod text;
 
-fn main() {
-    // Parse arguments.
-    let args = Args::parse();
-    if args.patterns.len() == 0 {
-        println!("No patterns were supplied!"); exit(1);
-    }
-    let patterns: Vec<&str> = args.patterns.iter().map(|p| p.as_str()).collect();
-
+fn run_fregex(args: &Args, patterns: &[&str]) {
     // Create proper matcher.
     let matcher: Box<dyn RegexMatcher> = if args.patterns.len() > 1 {
         match MultiRegex::new(&patterns) {
@@ -29,33 +23,74 @@ fn main() {
         }
     };
 
-    if args.singleline {
-        // Read text line-by-line and find every single match.
-        for line in read_text_lines(&args.file) {
-            if let Ok(line) = line {
-                if let Some(result) = matcher.find(&line) {
-                    println!("{}", result.as_str());
-                }
-            }
-        }
-    } else {
-        // Read text in whole and find every single match.
-        let string = read_text(&args.file);
-        let mut text: &str = &string;
-        let mut offset = 0;
 
-        while text.len() > 0 {
-            if let Some(result) = matcher.find(&text) {
-                let start = result.start() + offset;
-                let end = result.end() + offset;
+    // Read text in whole and find every single match.
+    let string = read_text(&args.file);
+    let mut text: &str = &string;
+    let mut offset = 0;
 
-                text = &text[result.end()..];
-                offset += result.end();
+    while text.len() > 0 {
+        if let Some(result) = matcher.find(&text) {
+            let start = result.start() + offset;
+            let end = result.end() + offset;
 
-                println!("({}, {})", start, end);
-            } else {
+            text = &text[result.end()..];
+            offset += result.end();
+
+            println!("({}, {})", start, end);
+
+            // If we only need the first match, break out of the loop here.
+            if args.first_only {
                 break;
             }
+        } else {
+            break;
         }
+    }
+}
+
+fn run_original(args: &Args, patterns: &[&str]) {
+    let matcher = match OriginalRegex::new(patterns[0]) {
+        Ok(matcher) => Box::new(matcher),
+        Err(_) => { println!("Error parsing patterns!"); exit(1); }
+    };
+
+    // Read text in whole and find every single match.
+    let string = read_text(&args.file);
+    let mut text: &str = &string;
+    let mut offset = 0;
+
+    while text.len() > 0 {
+        if let Some(result) = matcher.find(&text) {
+            let start = result.start() + offset;
+            let end = result.end() + offset;
+
+            text = &text[result.end()..];
+            offset += result.end();
+
+            println!("({}, {})", start, end);
+
+            // If we only need the first match, break out of the loop here.
+            if args.first_only {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+fn main() {
+    // Parse arguments.
+    let args = Args::parse();
+    if args.patterns.len() == 0 {
+        println!("No patterns were supplied!"); exit(1);
+    }
+    let patterns: Vec<&str> = args.patterns.iter().map(|p| p.as_str()).collect();
+
+    if args.original {
+        run_original(&args, &patterns);
+    } else {
+        run_fregex(&args, &patterns);
     }
 }
--- fregex_grep/src/text.rs
+++ fregex_grep/src/text.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{Read, BufReader, BufRead, Lines}, process::exit};
+use std::{fs::File, io::Read, process::exit};
 
 pub fn read_text(path: &str) -> String {
     if let Ok(mut file) = File::open(path) {
@@ -8,14 +8,6 @@ pub fn read_text(path: &str) -> String {
             Ok(_) => return string,
             Err(_) => { println!("Error reading input file!"); exit(1); }
         }
-    }
-
-    println!("Error opening input file!"); exit(1);
-}
-
-pub fn read_text_lines(path: &str) -> Lines<BufReader<File>> {
-    if let Ok(file) = File::open(path) {
-        return BufReader::new(file).lines();
     }
 
     println!("Error opening input file!"); exit(1);
